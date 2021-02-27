d2df4072916a2782c34a2cc12b1665497873471d
manchi17 <manseed.rt7@gmail.com>
2021-02-27 14:31:06 +0530

    Added bottem tab styles

--- src/common/navigator.js
+++ src/common/navigator.js
@@ -1,15 +1,35 @@
 import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import Search from "../screens/Search";
 import Welcome from "../screens/Welcome";
-import { NavigationContainer } from '@react-navigation/native';
+
 
 
 const Tab = createBottomTabNavigator();
 export default function BottomNavigator() {
     return (
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Welcome') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Search') {
+              iconName = 'search' ;
+            }
+            return <Ionicons name={iconName} size={size} color={"#00a2ed"} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'black',
+          inactiveTintColor: 'gray',
+        }}>
           <Tab.Screen name="Welcome" component={Welcome} />
           <Tab.Screen name="Search" component={Search} />
         </Tab.Navigator>
